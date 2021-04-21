50c45f8530177cf2fb6771759cf6fd66381f9964
arthurs14 <arthurcs426@gmail.com>
2021-04-21 16:05:59 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World, new app today!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
