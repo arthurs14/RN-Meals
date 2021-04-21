fd72ad15b1e9dbc97338f661f1496cd157e7ee16
arthurs14 <arthurcs426@gmail.com>
2021-04-21 16:30:42 -0700

    testing that fonts are working on both iOS and Android

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import { Text, View, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text>Hello World, new app today!</Text>
+      <Text style={styles.text}>Hello World, new app today!</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'OpenSans-Regular',
+  },
+});
 
 export default App;
