58e41d208cc3e603f470e5b0646ad46cbf959c4d
arthurs14 <arthurcs426@gmail.com>
2021-04-21 16:56:38 -0700

    added react navigation

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View>
-      <Text style={styles.text}>Hello World, new app today!</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.screen}>
+        <Text style={styles.text}>Hello World, new app today!</Text>
+      </View>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
   text: {
     fontFamily: 'OpenSans-Regular',
   },
