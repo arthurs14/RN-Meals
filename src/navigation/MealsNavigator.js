971c37d265f28b34acabe543827a2d454622296c
arthurs14 <arthurcs426@gmail.com>
2021-04-25 17:51:28 -0700

    added two more routes to our stack navigator

--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -3,13 +3,16 @@ import { createStackNavigator } from 'react-navigation/stack';
 
 import CategoriesScreen from '../screens/Categories';
 import CategoryMealsScreen from '../screens/CategoryMeals';
+import MealDetailScreen from '../screens/MealDetail';
 
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="Categories">
       <Stack.Screen name="Categories" component={CategoriesScreen} />
+      <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
+      <Stack.Screen name="MealDetail" component={MealDetailScreen} />
     </Stack.Navigator>
   );
 };
