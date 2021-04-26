910133855c08bff3a10971c558a0965545c196ba
arthurs14 <arthurcs426@gmail.com>
2021-04-25 18:05:55 -0700

    error with imports so should be fixed now

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './src/navigation/MealsNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <View style={styles.screen}>
-        <Text style={styles.text}>Hello World, new app today!</Text>
-      </View>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import CategoriesScreen from '../screens/Categories';
 import CategoryMealsScreen from '../screens/CategoryMeals';
