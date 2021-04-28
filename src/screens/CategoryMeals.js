334c333d6653d138e20afb8f10564e771d885d14
arthurs14 <arthurcs426@gmail.com>
2021-04-28 14:49:36 -0700

    filter through our meals data to find data that corresponds with the categoryId passed

--- src/screens/CategoryMeals.js
+++ src/screens/CategoryMeals.js
@@ -1,13 +1,17 @@
 import React, { useLayoutEffect } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, FlatList, Text, Button, StyleSheet } from 'react-native';
 
-import { CATEGORIES } from '../data/data';
+import { CATEGORIES, MEALS } from '../data/data';
 
 const CategoryMeals = ({ navigation, route }) => {
   const { categoryId } = route.params;
 
   const selectedCategory = CATEGORIES.find(
     category => category.id === categoryId,
+  );
+
+  const displayMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(categoryId) >= 0,
   );
 
   // Changes header styling and title to match the category
