4560ec035cde9da2ecaee783839f8427dcc10f00
arthurs14 <arthurcs426@gmail.com>
2021-05-02 14:23:49 -0700

    fixed an error where duration was outside meal description

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const MealItem = ({ title, onSelectMeal }) => {
+const MealItem = ({ title, duration, onSelectMeal }) => {
   return (
     <View style={styles.mealItem}>
       <TouchableOpacity onPress={onSelectMeal}>
         <View>
-          <View style={styles.meal}>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
             <Text>{title}</Text>
           </View>
-          <View style={styles.meal}></View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+            <Text>{duration} minutes</Text>
+          </View>
         </View>
       </TouchableOpacity>
     </View>
@@ -21,10 +23,15 @@ const styles = StyleSheet.create({
     height: 200,
     width: '100%',
     backgroundColor: '#ccc',
+    marginTop: 10,
   },
-  meal: {
+  mealRow: {
     flexDirection: 'row',
   },
+  mealHeader: {
+    height: '80%',
+  },
+  mealDetail: {},
 });
 
 export default MealItem;
--- src/screens/CategoryMeals.js
+++ src/screens/CategoryMeals.js
@@ -16,7 +16,13 @@ const CategoryMeals = ({ navigation, route }) => {
   );
 
   const renderMealItem = itemData => {
-    return <MealItem title={itemData.item.title} onSelectMeal={() => {}} />;
+    return (
+      <MealItem
+        title={itemData.item.title}
+        onSelectMeal={() => {}}
+        duration={itemData.item.duration}
+      />
+    );
   };
 
   // Changes header styling and title to match the category
@@ -32,6 +38,7 @@ const CategoryMeals = ({ navigation, route }) => {
         data={displayMeals}
         keyExtractor={(item, index) => item.id}
         renderItem={renderMealItem}
+        style={styles.list}
       />
     </View>
   );
@@ -42,6 +49,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  list: {
+    width: '90%',
   },
 });
 
