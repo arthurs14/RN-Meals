2855f1ba9940752ce5542211d14b3e91394ad150
arthurs14 <arthurcs426@gmail.com>
2021-05-02 14:40:53 -0700

    added a background image along with its styling

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
 
 const MealItem = ({ meal, onSelectMeal }) => {
+  const { imageUrl, title, duration, complexity, affordability } = meal.item;
+
   return (
     <View style={styles.mealItem}>
       <TouchableOpacity onPress={onSelectMeal}>
         <View>
           <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
-            <Text>{meal.item.title}</Text>
+            <ImageBackground source={{ uri: imageUrl }} style={styles.bgImage}>
+              <Text style={styles.title}>{title}</Text>
+            </ImageBackground>
           </View>
           <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-            <Text>{meal.item.duration} min</Text>
-            <Text>{meal.item.complexity.toUpperCase()}</Text>
-            <Text>{meal.item.affordability.toUpperCase()}</Text>
+            <Text>{duration} min</Text>
+            <Text>{complexity.toUpperCase()}</Text>
+            <Text>{affordability.toUpperCase()}</Text>
           </View>
         </View>
       </TouchableOpacity>
@@ -27,6 +37,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#ccc',
     marginTop: 10,
   },
+  bgImage: {
+    width: '100%',
+    height: '100%',
+  },
   mealRow: {
     flexDirection: 'row',
   },
@@ -36,6 +50,14 @@ const styles = StyleSheet.create({
   mealDetail: {
     paddingHorizontal: 10,
     justifyContent: 'space-between',
+  },
+  title: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 20,
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    paddingVertical: 4,
+    paddingHorizontal: 12,
   },
 });
 
