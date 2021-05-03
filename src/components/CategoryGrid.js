f32048994d9b947781779106baebbcd5655bfab2
arthurs14 <arthurcs426@gmail.com>
2021-05-03 15:33:49 -0700

    fixed styling to be more specific depending on platform

--- src/components/CategoryGrid.js
+++ src/components/CategoryGrid.js
@@ -32,7 +32,12 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow:
+      Platform.OS === 'android' && Platform.Version >= 21
+        ? 'hidden'
+        : 'visible',
+    // Affects Only Android
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -42,8 +47,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    // Affects Android
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
