3548cea0a020b725f429ffb37b50e9ffabceec6d
arthurs14 <arthurcs426@gmail.com>
2021-05-03 15:21:29 -0700

    made some changes to use icons and made a custom header button

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -1,2 +1,19 @@
 import React from 'react';
-// import {  } from 'react-native';
+import { Platform } from 'react-native';
+import { HeaderButton } from 'react-navigation-header-buttons';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import Colors from '../constants/Colors';
+
+const CustomHeaderButton = props => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Icon}
+      iconSize={23}
+      color={Platform.OS === 'android' ? 'white' : Colors.primaryColor}
+    />
+  );
+};
+
+export default CustomHeaderButton;
