ca0396dfebbd83b7870105f5e85c9485a0f36380
arthurs14 <arthurcs426@gmail.com>
2021-05-04 14:24:08 -0700

    added material bottom tab dependency

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import AllMealsStack from './MealsStackNavigator';
 import Favorites from '../screens/Favorites';
 import Colors from '../constants/Colors';
 
-const Tab = createBottomTabNavigator();
+const Tab =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator()
+    : createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
