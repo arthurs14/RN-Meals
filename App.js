ee06fc21f49b5362bb2079f6c973a345a445a6a2
arthurs14 <arthurcs426@gmail.com>
2021-05-03 17:19:19 -0700

    added icons and made it so it highlights which tab you are on

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import StackNavigator from './src/navigation/StackNavigator';
+import TabNavigator from './src/navigation/TabNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 };
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import AllMealsStack from './MealsStackNavigator';
+import Favorites from '../screens/Favorites';
+import Colors from '../constants/Colors';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="All Meals" />
-      <Tab.Screen name="My Favorites" />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: tabInfo => {
+          if (route.name === 'Meals') {
+            return (
+              <Icon name="ios-restaurant" size={25} color={tabInfo.color} />
+            );
+          } else if (route.name === 'My Favorites') {
+            return <Icon name="ios-star" size={25} color={tabInfo.color} />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: Colors.accentColor,
+        //inactiveTintColor: 'gray',
+      }}>
+      <Tab.Screen name="Meals" component={AllMealsStack} />
+      <Tab.Screen name="My Favorites" component={Favorites} />
     </Tab.Navigator>
   );
 };
