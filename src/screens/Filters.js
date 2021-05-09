b2ee78d1bcde4a5d326c5cd010f0c2a6fafcd2f8
arthurs14 <arthurcs426@gmail.com>
2021-05-09 15:10:20 -0700

    fixed drawer menu icon

--- src/screens/Filters.js
+++ src/screens/Filters.js
@@ -1,10 +1,22 @@
 import React, { useLayoutEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
 
 const Filters = ({ navigation }) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Filtered Meals',
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => navigation.toggleDrawer()}
+          />
+        </HeaderButtons>
+      ),
     });
   }, [navigation]);
 
