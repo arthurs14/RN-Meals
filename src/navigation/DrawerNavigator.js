e51d83f24783459dd18ef4d429d237c150dca374
arthurs14 <arthurcs426@gmail.com>
2021-05-10 16:07:57 -0700

    added styling to drawer navigator

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
 
 import TabNavigator from './TabNavigator';
 import FilterScreen from '../screens/Filters';
+import { screenOptions } from './StackSettings';
 
 const Drawer = createDrawerNavigator();
 
-const FilteredNavigation = createStackNavigator();
-
-const FilterNavigator = () => (
-  <FilteredNavigation>
-    <FilteredNavigation.Screen name="Filters" component={FilterScreen} />
-  </FilteredNavigation>
-);
-
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator screenOptions={screenOptions}>
       <Drawer.Screen
         name="MealsFavs"
         component={TabNavigator}
