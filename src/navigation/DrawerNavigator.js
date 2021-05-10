324f8e56db58fb0223cd4d3fbb2784553a3497ae
arthurs14 <arthurcs426@gmail.com>
2021-05-10 16:33:15 -0700

    when clicking on label in drawer - highlights the active drawer label

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import TabNavigator from './TabNavigator';
 import FilterScreen from '../screens/Filters';
 import { screenOptions } from './StackSettings';
+import Colors from '../constants/Colors';
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Filters Meal" component={FilterScreen} />
+    </Stack.Navigator>
+  );
+};
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator screenOptions={screenOptions}>
+    <Drawer.Navigator
+      screenOptions={{
+        ...screenOptions,
+        drawerActiveTintColor: Colors.accentColor,
+        drawerActiveBackgroundColor: 'white',
+      }}>
       <Drawer.Screen
         name="MealsFavs"
         component={TabNavigator}
@@ -18,7 +35,14 @@ const DrawerNavigator = () => {
           drawerLabel: 'Meals',
         }}
       />
-      <Drawer.Screen name="Filters" component={FilterScreen} />
+      <Drawer.Screen
+        name="Filters"
+        component={StackNavigator}
+        options={{
+          headerShown: false,
+          drawerLabel: 'Filters',
+        }}
+      />
     </Drawer.Navigator>
   );
 };
