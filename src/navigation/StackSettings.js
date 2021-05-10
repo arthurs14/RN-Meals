ac24035831a20c4818e34a732a2ef84649fdd3df
arthurs14 <arthurcs426@gmail.com>
2021-05-10 16:50:50 -0700

    fixed font style on header and back header buttons

--- src/navigation/StackSettings.js
+++ src/navigation/StackSettings.js
@@ -6,5 +6,11 @@ export const screenOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'white',
   },
+  headerTitleStyle: {
+    fontFamily: 'OpenSans-Bold',
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'OpenSans-Regular',
+  },
   headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
 };
